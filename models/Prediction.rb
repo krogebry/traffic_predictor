c5753fedd261179d449f5b390d7c903d64eca5a3
Bryan Kroger <bryan.kroger@gmail.com>
2010-10-22 18:10:37 -0700

    Creating a google maps mashup, needs more work as far as directional arrows.

---
+++ models/Prediction.rb
@@ -0,0 +1,115 @@
+##
+# Prediction
+##
+
+class Prediction
+	@todHour
+	@todMinute
+
+	@highway
+	@direction
+
+	attr_accessor :todHour, :todMinute, :highway, :direction
+	def initialize()
+	end
+
+	def getCursor()
+		DBConn.collection( "segment_states" ).find({
+			:location => @highway.to_i()
+		},{
+			:sort => [["milepost","DESC"]]
+		})
+	end
+
+	def getSegments()
+		c = self.getCursor()
+		ro = []
+
+		c.each do |segment|
+			next if(segment["loc"][0] == 0)
+			ro.push( segment )
+		end
+
+		return ro
+	end
+
+	##
+	# 0: nil
+	# 1: green ( 0000ff )
+	# 2: yellow ( ffdd00 )
+	# 3: pink ( ffcccc )
+	# 4: red ( ff0000 )
+	##
+	def getColor( readings )
+		total = 0
+		readings.map(){|r| total+=r }
+		return "#000000" if(total == 0)
+		#Log.debug( "Total" ){ total }
+		#Log.debug( "Reading" ){ readings.inspect }
+
+		baseColor = [255,255,255]
+
+		p = (( readings[1].to_f / total)*255)
+		#puts "Green: #{p}"
+		baseColor[0] -= p.to_i()
+		baseColor[2] -= p.to_i()
+
+		p = (( readings[2].to_f / total)*255)
+		baseColor[0] += (p*0.75).to_i()
+		baseColor[1] += (p*0.25).to_i()
+		baseColor[2] -= p.to_i()
+
+		p = (( readings[4].to_f / total)*255)
+		baseColor[0] += p.to_i()
+		baseColor[1] -= p.to_i()
+		baseColor[2] -= p.to_i()
+
+		p = (( readings[4].to_f / total)*255)
+		baseColor[0] += p.to_i()
+		baseColor[1] -= p.to_i()
+		baseColor[2] -= p.to_i()
+
+		baseColor.map!{|bc| (bc > 255 ? 255 : (bc < 0 ? 0 : bc )) }
+
+		#red = ( baseColor[0] == 0 ? "00" : baseColor[0].to_s( 16 ) )
+		#green = ( baseColor[1] == 0 ? "00" : baseColor[1].to_s( 16 ) )
+		#blue = ( baseColor[2] == 0 ? "00" : baseColor[2].to_s( 16 ) )
+
+		return "#"+
+			baseColor[0].to_s( 16 ).rjust(2,'0') + 
+			baseColor[1].to_s( 16 ).rjust(2,'0') + 
+			baseColor[2].to_s( 16 ).rjust(2,'0')
+	end
+
+	def getPredictions()
+		i = 0
+		ro = []
+
+		segments = self.getSegments()
+
+		segments.each do |segment|
+			segNext = segments[i+1]
+			#Log.debug( "Next" ){ segments[i+1].inspect }
+
+			if(segNext != nil)
+
+				readings = segment["readings"][@todHour][@todMinute]
+				#Log.debug( "Readings" ){ readings.inspect }
+
+				color = self.getColor( readings )
+
+
+				ro.push({
+					:start => segment["loc"],
+					:name => segment["title"],
+					:end => segNext["loc"],
+					:color => color
+				})
+			end
+			i+=1
+		end
+
+		return ro
+	end
+
+end
