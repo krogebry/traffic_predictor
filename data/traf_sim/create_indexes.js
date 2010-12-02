1b547cac27ed150212090324f9703849f3f88cc5
krogebry <bryan.kroger@gmail.com>
2010-12-02 10:07:25 -0800

    Adding more to the reload process.

---
+++ data/traf_sim/create_indexes.js
@@ -0,0 +1,4 @@
+db.segment_states.ensureIndex({ guid: 1 });
+db.segment_states.ensureIndex({ location: 1 });
+db.segment_states.ensureIndex({ milepost: 1 });
+db.segment_states.ensureIndex({ loc: "2d" });
